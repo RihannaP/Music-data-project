b8ed1e79d91111da195edf132913b2ec5551cfab
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-02-16 17:01:45 +0000

    Implement populateDropdown function

--- script.js
+++ script.js
@@ -4,9 +4,31 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIDs } from "./data.js";
+import { getUserIDs, getListenEvents, getSong } from "./data.js";
 
 window.onload = function () {
-  const users = getUserIDs();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
+  populateUserDropdown()
+  
 };
+
+function populateUserDropdown() {
+  const userSelect = document.getElementById("user-select");
+  const users = getUserIDs();
+
+  // default
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "--Select a user--";
+  userSelect.appendChild(defaultOption);
+
+  // Populate dropdown with user options
+  users.forEach((userId) => {
+    const option = document.createElement("option");
+    option.value = userId;
+    option.textContent = `User ${userId}`;
+    userSelect.appendChild(option);
+  });
+}
+
+
+
